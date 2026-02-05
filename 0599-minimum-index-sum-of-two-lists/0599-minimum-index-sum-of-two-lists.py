b235d898bfdfbf616c6c5960e807b36f51ff446a
Yoseph Kedir <yoseph.kedir10@gmail.com>
2026-02-05 14:38:38 +0300

    Time: 102 ms (22.19%), Space: 19.7 MB (47.38%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -3,15 +3,11 @@ class Solution:
         low_index = float('inf')
         word = []
         for i in list2:
-            print("i:", i)
             if i in list1:
                 tot = list2.index(i) + list1.index(i)
-                print("tot:", tot)
-                print("low_index:", low_index)
                 if tot == low_index:
                     low_index = tot
                     word.append(i)
-                    print("word:", word)
                 elif tot < low_index:
                     low_index = tot
                     word = [i]
