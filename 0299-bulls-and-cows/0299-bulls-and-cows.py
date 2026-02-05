10a5995043c79c71f616eb74d84e5a52ddc4f1c6
Yoseph Kedir <yoseph.kedir10@gmail.com>
2026-02-05 11:49:02 +0300

    Time: 7 ms (47.22%), Space: 19.4 MB (20.83%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.py
@@ -0,0 +1,23 @@
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        secret_freq, guess_freq = {}, {}
+        correct_guesses, inplace_guesses = 0, 0
+        for i in range(len(secret)):
+            secret_freq[secret[i]] = secret_freq.get(secret[i], 0) + 1
+            guess_freq[guess[i]] = guess_freq.get(guess[i], 0) + 1
+        print("sec:", secret_freq)
+        print("gus:", guess_freq)
+        for i in guess_freq:
+            if guess_freq[i] >= secret_freq.get(i, 0):
+                correct_guesses += secret_freq.get(i, 0)
+            else:
+                correct_guesses += guess_freq.get(i, 0)
+        for i in range(len(guess)):
+            if guess[i] == secret[i]:
+                inplace_guesses += 1
+        return f"{inplace_guesses}A{correct_guesses - inplace_guesses}B"
+        
+            
+            
+
+        
