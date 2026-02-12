413b6cbe94a2e110f003589db5ec043eeb94d81c
Yoseph Kedir <yoseph.kedir10@gmail.com>
2026-02-12 17:33:42 +0300

    Time: 24 ms (5.79%), Space: 13.7 MB (6.21%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,18 +1,31 @@
 class Solution(object):
     def setZeroes(self, matrix):
-        rows = set()
-        cols = set()
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        m = len(matrix[0])
+
+        for i in range(n):
+            for j in range(m):
+
                 if matrix[i][j] == 0:
-                    cols.add(i)
-                    rows.add(j)
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if i in cols or j in rows:
+                    for k in range(m):
+                        if matrix[i][k] != 0:
+                            matrix[i][k] = float("inf")
+
+                    for k in range(n):
+                        if matrix[k][j] != 0:
+                            matrix[k][j] = float("inf")
+
+
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == float("inf"):
                     matrix[i][j] = 0
 
-
-
         
+
+
         
