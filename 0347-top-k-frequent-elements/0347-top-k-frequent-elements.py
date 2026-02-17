6ba502b1a07e831935daac539394e4ad7b321f36
Yoseph Kedir <yoseph.kedir10@gmail.com>
2026-02-17 22:41:13 +0300

    Time: 9 ms (68.4%), Space: 14.2 MB (76.42%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -4,10 +4,9 @@ class Solution(object):
         count = Counter(nums)
         rev_freq = {}
         for i in count:
-            if count[i] in rev_freq:
-                rev_freq[count[i]].append(i)
-            else:
-                rev_freq[count[i]] = [i]
+            if count[i] not in rev_freq:
+                rev_freq[count[i]] = []
+            rev_freq[count[i]].append(i)
         for i in sorted(rev_freq, reverse=True):
             res.extend(rev_freq[i])
 
