8c83362440323e8eb914c1fc7c79118f14f072e4
Yoseph Kedir <yoseph.kedir10@gmail.com>
2026-02-18 12:42:24 +0300

    Time: 10 ms (32.63%), Space: 12.3 MB (91.47%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -5,16 +5,15 @@ class Solution(object):
         box = defaultdict(set)
         for i in range(9):
             for j in range(9):
-                if board[i][j] != ".":
-                    if board[i][j] in col[i]:
-                        return False
-                    elif board[i][j] in row[j]:
-                        return False
-                    elif board[i][j] in box[(i // 3, j // 3)]:
-                        return False
-                    row[j].add(board[i][j])
-                    col[i].add(board[i][j])
-                    box[(i // 3, j // 3)].add(board[i][j])
+                if board[i][j] == ".":
+                    continue
+                    
+                if board[i][j] in col[i] or board[i][j] in row[j] or board[i][j] in box[(i // 3, j // 3)]:
+                    return False
+
+                row[j].add(board[i][j])
+                col[i].add(board[i][j])
+                box[(i // 3, j // 3)].add(board[i][j])
 
         return True
 
