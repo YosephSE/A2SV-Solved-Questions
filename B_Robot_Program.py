c9c12d2a2d3e14437704a20f5366c29753e0f7e3
YosephSE <yoseph.kedir10@gmail.com>
2026-02-18 21:01:07 +0300

    robot program

---
+++ B_Robot_Program.py
@@ -0,0 +1,45 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n, x, k = map(int, input().split())
+    s = input().strip()
+    
+    pref = 0
+    first_hit = -1
+
+    for i in range(n):
+        if s[i] == 'L':
+            pref -= 1
+        else:
+            pref += 1
+        
+        if x + pref == 0:
+            first_hit = i + 1
+            break
+    
+    if first_hit == -1 or first_hit > k:
+        print(0)
+        continue
+    
+    ans = 1
+    remaining = k - first_hit
+    
+    pref = 0
+    cycle_len = -1
+    
+    for i in range(n):
+        if s[i] == 'L':
+            pref -= 1
+        else:
+            pref += 1
+        
+        if pref == 0:
+            cycle_len = i + 1
+            break
+    
+    if cycle_len != -1:
+        ans += remaining // cycle_len
+    
+    print(ans)
