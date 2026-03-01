610f6bb7f12a67a0e297f2befe04ccaf7f2ad707
YosephSE <yoseph.kedir10@gmail.com>
2026-03-01 22:33:23 +0300

    hackerrank bubble sort

--- E_Minimizing_Inversions_in_Concatenated_Arrays.py
+++ E_Minimizing_Inversions_in_Concatenated_Arrays.py
@@ -1,7 +1,7 @@
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    a = []
-    for i in range(n):
-        a.append(list(map(int, input().split())))
-    a = sorted(a, lambda i[0], i[1]: i[0] + i[1])
+# t = int(input())
+# for _ in range(t):
+#     n = int(input())
+#     a = []
+#     for i in range(n):
+#         a.append(list(map(int, input().split())))
+#     a = sorted(a, lambda i[0], i[1]: i[0] + i[1])
