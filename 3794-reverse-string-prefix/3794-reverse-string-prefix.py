d6010cb9c260cdbf899854d599225b8ab662d01c
Yoseph Kedir <yoseph.kedir10@gmail.com>
2026-03-02 23:37:55 +0300

    Time: 0 ms (100%), Space: 12.4 MB (89.18%) - LeetHub

--- 3794-reverse-string-prefix/3794-reverse-string-prefix.py
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.py
@@ -1,7 +1,5 @@
 class Solution(object):
     def reversePrefix(self, s, k):
-        s1 = list(s[:k])
-        s1.reverse()
-        return "".join(s1) + s[k:]
+        return s[:k][::-1] + s[k:]
 
         
