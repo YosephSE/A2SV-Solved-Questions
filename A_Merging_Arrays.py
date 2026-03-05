aab6b6f1e13e20ad4ea18811ed3b9f7af7389fcb
YosephSE <yoseph.kedir10@gmail.com>
2026-03-06 00:21:16 +0300

    merging arrays

---
+++ A_Merging_Arrays.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+# res = []
+# if a[0] > b[0]:
+#     a, b = b, a
+
+# p1, p2 = 
+
+
+p = a + b
+p.sort()
+
+print(*p)
