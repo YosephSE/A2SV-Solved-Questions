997f8027ee207b4351452436d40316bc41f0dbde
YosephSE <yoseph.kedir10@gmail.com>
2026-03-06 00:54:19 +0300

    merging array optimized code

--- A_Merging_Arrays.py
+++ A_Merging_Arrays.py
@@ -1,14 +1,26 @@
 n, k = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
-# res = []
-# if a[0] > b[0]:
-#     a, b = b, a
 
-# p1, p2 = 
+ap, bp = 0, 0
+res = []
+while True:
+    if ap >= len(a) and bp >= len(b):
+        break
+    elif ap >= len(a):
+        res.append(b[bp])
+        bp += 1
+    elif bp >= len(b):
+        res.append(a[ap])
+        ap += 1
+    else:
+        if a[ap] < b[bp] :
+            res.append(a[ap])
+            ap += 1
+        elif a[ap] >= b[bp]:
+            res.append(b[bp])
+            bp += 1
 
 
-p = a + b
-p.sort()
 
-print(*p)
+print(* res)
