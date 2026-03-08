1b80e9aeace9dec11ef6bb2eb874c90a4af2160d
Yoseph Kedir <yoseph.kedir10@gmail.com>
2026-03-09 01:06:24 +0300

    Time: 7 ms (51.08%), Space: 12.8 MB (97.13%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -6,9 +6,7 @@ class Solution(object):
                 return [l + 1, r + 1]
             elif nums[l] + nums[r] > target:
                 r -= 1
-            else:
+            elif nums[l] + nums[r] < target:
                 l += 1
                 
-            
-        
         
