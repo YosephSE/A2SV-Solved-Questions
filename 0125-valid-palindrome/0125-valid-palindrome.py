081b0df0948c386eb6927a0760655638ca33d27a
Yoseph Kedir <yoseph.kedir10@gmail.com>
2026-03-09 00:43:38 +0300

    Time: 29 ms (32.07%), Space: 12.7 MB (87.63%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,15 +2,11 @@ class Solution(object):
     def isPalindrome(self, s):
         l, r = 0, len(s) - 1
         while l <= r:
-            print(l, r)
             if ord(s[l]) < 48 or 57 < ord(s[l]) < 65 or 90 < ord(s[l]) < 97 or ord(s[l]) > 122:
-                print(1)
                 l += 1
             elif ord(s[r]) < 48 or 57 < ord(s[r]) < 65 or 90 < ord(s[r]) < 97 or ord(s[r]) > 122:
-                print(2)
                 r -= 1
             else:
-                print(3)
                 if ord(s[l].lower()) != ord(s[r].lower()):
                     return False
                 l += 1
