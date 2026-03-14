0be205e0c1588d9dddf68ac24170b62fcbf5d903
Yoseph Kedir <yoseph.kedir10@gmail.com>
2026-03-15 00:35:56 +0300

    Time: 2131 ms (30.43%), Space: 13.3 MB (47.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,8 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        visited = {}
         for i in range(len(nums)):
-            if target - nums[i] in visited:
-                return [visited[target - nums[i]], i]
-            visited[nums[i]] = i
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
         
