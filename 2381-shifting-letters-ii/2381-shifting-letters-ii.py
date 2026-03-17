d6656d7ae27c4f5056fe9c6c3a216276d77f944a
Yoseph Kedir <yoseph.kedir10@gmail.com>
2026-03-18 00:34:59 +0300

    Time: 91 ms (78.57%), Space: 30.5 MB (42.86%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.py
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -1,17 +1,19 @@
 class Solution(object):
     def shiftingLetters(self, s, shifts):
-        prefix = [0] * len(s)
+        n = len(s)
+        prefix = [0] * (n + 1)
         res = []
-        for shift in shifts:
+        base = ord('a')
 
-            prefix[shift[0]] += (1 if shift[2] == 1 else -1)
-            if len(prefix) - 1 >= shift[1] + 1:
-                prefix[shift[1] + 1] += (-1 if shift[2] == 1 else 1)
-        for i in range(1, len(prefix)):
+        for left, right, direction in shifts:
+            prefix[left] += 1 if direction == 1 else -1
+            prefix[right + 1] += -1 if direction == 1 else 1
+
+        for i in range(1, n):
             prefix[i] += prefix[i - 1]
 
-        for i in range(len(s)):
-            ord_val = (ord(s[i]) - ord('a') + prefix[i]) % 26 + ord('a')
-            char = chr(ord_val)
-            res.append(char)
+        for i in range(n):
+            ord_val = (ord(s[i]) - base + prefix[i]) % 26 + base
+            res.append(chr(ord_val))
+
         return ''.join(res)
