8a3e7640cb461a97ada55d912c070bca5f11b9e8
Yoseph Kedir <yoseph.kedir10@gmail.com>
2026-03-19 16:06:44 +0300

    Time: 11 ms (32.02%), Space: 13.6 MB (12.39%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -7,11 +7,8 @@ class Solution(object):
 
         for i in range(1, len(nums)):
             prefix.append(prefix[-1] + nums[i])
-        # prefix.append(0)
-        print(prefix)
-        
+
         for i in range(1, len(prefix)):
-            print(i)
             if total - prefix[i] == prefix[i - 1]:
                 return i - 1
         return -1
