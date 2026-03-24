7832cc2fb1d22253ded8098c093d9cb1fdd6911f
Yoseph Kedir <yoseph.kedir10@gmail.com>
2026-03-24 10:40:50 +0300

    Time: 44 ms (34.99%), Space: 12.7 MB (82.22%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        def compareCounter(c1, c2):
+            for c in c1:
+                if c1[c] != c2[c]:
+                    return False
+            return True
+        s1_count = Counter(s1)
+        s2_count = Counter(s2[:len(s1)])
+        if compareCounter(s1_count, s2_count):
+            return True
+        l = 0
+        for r in range(len(s1), len(s2)):
+            s2_count[s2[l]] -= 1
+            # if s2_count[l] == 0:
+            #     s2_count.pop()
+                
+            l += 1
+            s2_count[s2[r]] += 1
+
+            if compareCounter(s1_count, s2_count):
+                return True
+        return False
+
+
