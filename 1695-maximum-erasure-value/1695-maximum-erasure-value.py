330b17694ffd57a83361e9467bb740a5409afc9d
Yoseph Kedir <yoseph.kedir10@gmail.com>
2026-03-24 20:36:30 +0300

    Time: 368 ms (16.28%), Space: 21.7 MB (5.43%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.py
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.py
@@ -1,26 +1,22 @@
+from collections import defaultdict
+
 class Solution(object):
     def maximumUniqueSubarray(self, nums):
         res = curr = 0
         unique = defaultdict(int)
         l = 0
+
         for r in range(len(nums)):
             curr += nums[r]
             unique[nums[r]] += 1
+
             while unique[nums[r]] > 1:
                 unique[nums[l]] -= 1
                 curr -= nums[l]
                 if unique[nums[l]] == 0:
-                    unique.pop(nums[l])
+                    del unique[nums[l]]
                 l += 1
+
             res = max(res, curr)
-        return res                
-                
 
-
-
-
-
-
-
-        
-        
+        return res
