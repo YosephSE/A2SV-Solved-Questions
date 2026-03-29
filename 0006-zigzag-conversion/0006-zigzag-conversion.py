07fd52eee003fde0a6f61db32c4e35d85ce63bae
Yoseph Kedir <yoseph.kedir10@gmail.com>
2026-03-29 21:36:59 +0300

    Time: 27 ms (12.85%), Space: 12.6 MB (41.76%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -3,13 +3,13 @@ class Solution:
         if numRows == 1:
             return s
 
-        res = ""
+        res = []
         for r in range(numRows):
             increment = 2 * (numRows - 1)
             for i in range(r, len(s), increment):
-                res += s[i]
+                res.append(s[i])
                 if (r > 0 and r < numRows - 1 and
                         i + increment - 2 * r < len(s)):
-                    res += s[i + increment - 2 * r]
+                    res.append(s[i + increment - 2 * r])
 
-        return res
+        return ''.join(res)
