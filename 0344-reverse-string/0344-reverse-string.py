b9b1a2a44980932f96808768cb9fa9c5bbcd4422
Yoseph Kedir <yoseph.kedir10@gmail.com>
2026-04-14 15:40:44 +0300

    Time: 277 ms (5.19%), Space: 44.9 MB (5.35%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,10 +1,19 @@
 class Solution(object):
     def reverseString(self, s):
         l, r = 0, len(s) - 1
-        while l < r:
+        
+        def switchChars(l, r):
+            if l >= r:
+                return
+            print(l, r)
             s[l], s[r] = s[r], s[l]
             l += 1
             r -= 1
+            switchChars(l, r)
+        switchChars(l, r)
+
+            
+
         
         
         
