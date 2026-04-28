cc8fdb6244cd506228e5c78babb50f10696c5229
Yoseph Kedir <yoseph.kedir10@gmail.com>
2026-04-29 00:07:34 +0300

    Time: 3 ms (3.44%), Space: 12.5 MB (38.32%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -4,17 +4,16 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution(object):
     def preorderTraversal(self, root):
         res = []
-
         def dfs(node):
             if not node:
                 return
             res.append(node.val)
             dfs(node.left)
             dfs(node.right)
-
         dfs(root)
         return res
+
+        
